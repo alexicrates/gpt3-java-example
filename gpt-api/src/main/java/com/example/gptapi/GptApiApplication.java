960b2f17267a9ab45583fbba3574eb616cb16b76
abobus <maleichuck.sasha@gmail.com>
2023-05-04 18:13:59 +0300

    tts python scripts changes

--- gpt-api/src/main/java/com/example/gptapi/GptApiApplication.java
+++ gpt-api/src/main/java/com/example/gptapi/GptApiApplication.java
@@ -1,7 +1,5 @@
 package com.example.gptapi;
 
-import com.example.gptapi.services.speaker.SoundPlayer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,14 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GptApiApplication implements CommandLineRunner {
 
-    SoundPlayer soundPlayer = new SoundPlayer();
-
     public static void main(String[] args) {
         SpringApplication.run(GptApiApplication.class, args);
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
 
     }
 }
