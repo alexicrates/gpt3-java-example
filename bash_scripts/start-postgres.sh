0213fc8a6369ff3b59851bee7b2d18046f442e81
abobus <maleichuck.sasha@gmail.com>
2023-06-06 12:17:36 +0300

    renamed docker-compose-only-postgres.yaml to postgres.yaml

--- bash_scripts/start-postgres.sh
+++ bash_scripts/start-postgres.sh
@@ -2,4 +2,4 @@
 
 project_dir=$(cd $(dirname "$0") && cd .. && pwd)
 docker pull postgres;
-docker compose --file "$project_dir"/docker-compose-only-postgres.yaml up --detach
+docker compose --file "$project_dir"/postgres.yaml up --detach
